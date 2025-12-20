10bb360570897a2af4d9b88565ee3e9d093e9b9e
Heber Pereira <isaiahpereira88@gmail.com>
2025-12-20 14:03:56 -0800

    setup canva and db schema

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -23,18 +23,16 @@ const Page = async ({ params }: PageProps) => {
   prefetchWorkflow(workflowId); // populate cache
 
   return (
-    <div>
-      <HydrateClient>
-        <ErrorBoundary fallback={<EditorError />}>
-          <Suspense fallback={<EditorLoading />}>
-            <EditorHeader workflowId={workflowId} />
-            <main className="flex-1">
-              <Editor workflowId={workflowId} />
-            </main>
-          </Suspense>
-        </ErrorBoundary>
-      </HydrateClient>
-    </div>
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1">
+            <Editor workflowId={workflowId} />
+          </main>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
   );
 };
 
